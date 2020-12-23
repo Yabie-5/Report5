2d85c46d83a85abff524ed7a1be786ba1dc2bba5
Takenari YABIKU <205744@ie.u-ryukyu.ac.jp>
2020-12-23 15:04:04 +0900

    end for Step2 of Report5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args){
+        try {
+            String str = null;
+            System.out.println(str.length());
+
+        } catch (NullPointerException e) {
+            System.out.println("エラー:NullpointerExceptionが発生しました。\n"+ e.getMessage());
+        }
     }
 }
